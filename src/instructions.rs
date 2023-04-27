a8a4451bdb020c5bff6b7e047693c858a90344b9
sathiarith <sathiarith_chau@uri.edu>
2023-04-27 12:08:49 -0400

    added more tests

--- src/instructions.rs
+++ src/instructions.rs
@@ -5,50 +5,50 @@ use std::process;
 use std::vec;
 
 /// Conditional Move: if $r[C] 6= 0 then $r[A] := $r[B]
-pub fn cmov(um: &mut UniversalMachine, A: &u32, B: &u32, C: &u32) {
-    if um.registers[*C as usize] != 0 {
-        um.registers[*A as usize] = um.registers[*B as usize];
+pub fn cmov(um: &mut UniversalMachine, r_a: &u32, r_b: &u32, r_c: &u32) {
+    if um.registers[*r_c as usize] != 0 {
+        um.registers[*r_a as usize] = um.registers[*r_b as usize];
     }
 }
 
 // Segmented Load: $r[A] := mem[$r[B]][$r[C]]
-pub fn seg_load(um: &mut UniversalMachine, A: &u32, B: &u32, C: &u32) {
-    let rb_val = um.registers[*B as usize] as usize;
-    let rc_val = um.registers[*C as usize] as usize;
-    um.registers[*A as usize] = um.mem_segs[rb_val][rc_val];
+pub fn seg_load(um: &mut UniversalMachine, r_a: &u32, r_b: &u32, r_c: &u32) {
+    let r_b_data = um.registers[*r_b as usize] as usize;
+    let r_c_data = um.registers[*r_c as usize] as usize;
+    um.registers[*r_a as usize] = um.mem_segs[r_b_data][r_c_data];
 }
 
 /// Segmented Store: mem[$r[A]][$r[B]] := $r[C]
-pub fn seg_store(um: &mut UniversalMachine, A: &u32, B: &u32, C: &u32) {
-    let ra_val = um.registers[*A as usize] as usize;
-    let rb_val = um.registers[*B as usize] as usize;
-    let rc_val = um.registers[*C as usize];
-    um.mem_segs[ra_val][rb_val] = rc_val;
+pub fn seg_store(um: &mut UniversalMachine, r_a: &u32, r_b: &u32, r_c: &u32) {
+    let r_a_data = um.registers[*r_a as usize] as usize;
+    let r_b_data = um.registers[*r_b as usize] as usize;
+    let r_c_data = um.registers[*r_c as usize];
+    um.mem_segs[r_a_data][r_b_data] = r_c_data;
 }
 
 /// Addition: $r[A] := ($r[B] + $r[C]) mod 2^32
-pub fn add(um: &mut UniversalMachine, A: &u32, B: &u32, C: &u32) {
-    um.registers[*A as usize] = um.registers[*B as usize].wrapping_add(um.registers[*C as usize]);
+pub fn add(um: &mut UniversalMachine, r_a: &u32, r_b: &u32, r_c: &u32) {
+    um.registers[*r_a as usize] = um.registers[*r_b as usize].wrapping_add(um.registers[*r_c as usize]);
 }
 
 /// Multiplication: $r[A] := ($r[B] * $r[C]) mod 2^32
-pub fn mul(um: &mut UniversalMachine, A: &u32, B: &u32, C: &u32) {
-    um.registers[*A as usize] = um.registers[*B as usize].wrapping_mul(um.registers[*C as usize]); 
+pub fn mul(um: &mut UniversalMachine, r_a: &u32, r_b: &u32, r_c: &u32) {
+    um.registers[*r_a as usize] = um.registers[*r_b as usize].wrapping_mul(um.registers[*r_c as usize]); 
 }
 
 /// Division: $r[A] := $r[B] div $r[C] (integer division)
-pub fn div(um: &mut UniversalMachine, A: &u32, B: &u32, C: &u32) {
-    if um.registers[*C as usize] == 0 {
+pub fn div(um: &mut UniversalMachine, r_a: &u32, r_b: &u32, r_c: &u32) {
+    if um.registers[*r_c as usize] == 0 {
         //print to stderr
         eprintln!("Error: division by zero");
     }
 
-    um.registers[*A as usize] = um.registers[*B as usize].wrapping_div(um.registers[*C as usize]);
+    um.registers[*r_a as usize] = um.registers[*r_b as usize].wrapping_div(um.registers[*r_c as usize]);
 }
 
 /// Bitwise NAND: $r[A] := not ($r[B] and $r[C])
-pub fn nand(um: &mut UniversalMachine, A: &u32, B: &u32, C: &u32) {
-    um.registers[*A as usize] = !(um.registers[*B as usize] & um.registers[*C as usize]);
+pub fn nand(um: &mut UniversalMachine, r_a: &u32, r_b: &u32, r_c: &u32) {
+    um.registers[*r_a as usize] = !(um.registers[*r_b as usize] & um.registers[*r_c as usize]);
 }
 
 /// Halt: stop execution and terminate the program
@@ -62,46 +62,37 @@ pub fn halt() {
 /// that is not all zeroes and does not identify any
 /// currently mapped segment is placed in $r[B].
 /// The new segment is mapped as $m[$r[B]].
-/// check validity of below code.
-pub fn map_seg(um: &mut UniversalMachine, B: &u32, C: &u32) {
-    // B is where we put the index storing the new segment
-    // C is our new segment word length
+pub fn map_seg(um: &mut UniversalMachine, r_b: &u32, r_c: &u32) {
 
-    // Create a new vector with r[C] words
-    let rc_val = um.registers[*C as usize];
-    let new_segment = vec![0_u32; rc_val as usize];
+    // new vector of 0s with length r_c
+    let r_c_data = um.registers[*r_c as usize];
+    let new_segment = vec![0_u32; r_c_data as usize];
 
     // Check if we already have any unmapped mem_segs
     if um.unmap_segs.len() > 0 {
-        // If we do have an unmapped segment:
-        // -Push new segment vector to the unmapped segment index
-        // -Store the unmapped segment's index in r[B]
-        // let unmapped_seg_index = *(um.unmap_segs.get(0).unwrap());
+        // remove it from the unmapped list
+        // update the mem_segs list and r[B]
         let unmapped_seg_index = um.unmap_segs.pop().unwrap();
         um.mem_segs[unmapped_seg_index as usize] = new_segment;
-        um.registers[*B as usize] = unmapped_seg_index;
+        um.registers[*r_b as usize] = unmapped_seg_index;
     } else {
-        // If we don't have any empty mem_segs, push a new one to the mem_segs vec
         um.mem_segs.push(new_segment);
-
-        // The new segment index is the length of the mem_segs vec
-        um.registers[*B as usize] = (um.mem_segs.len() - 1) as u32;
+        um.registers[*r_b as usize] = (um.mem_segs.len() - 1) as u32;
     }
 }
 
 /// The segment identified by $r[C] is unmapped.
 /// Future Map Segment instructions may reuse the identifier $r[C].
-pub fn unmap_seg(um: &mut UniversalMachine, C: &u32) {
-    // To unmap a segment, simply add its index to the unmap_segs vector
-    um.unmap_segs.push(um.registers[*C as usize]);
+pub fn unmap_seg(um: &mut UniversalMachine, r_c: &u32) {
+    // tracker for unmapped segments
+    um.unmap_segs.push(um.registers[*r_c as usize]);
 }
 
 // The value in $r[C] is displayed on the console immediately.
 // Only values between and including 0 and 255 are allowed.
-pub fn output(um: &mut UniversalMachine, C: &u32) {
-    // print!("{}", char::from_u32(um.r[*C as usize]).unwrap());
-    let r = u8::try_from(um.registers[*C as usize]).unwrap();
-    print!("{}", r as char);
+pub fn output(um: &mut UniversalMachine, r_c: &u32) {
+    let out = u8::try_from(um.registers[*r_c as usize]).unwrap();
+    print!("{}", out as char);
 }
 
 /// The um waits for input on the I/O device. When
@@ -109,10 +100,10 @@ pub fn output(um: &mut UniversalMachine, C: &u32) {
 /// which must be a value from 0 to 255. If the end
 /// of input has been signaled, then $r[C] is loaded
 /// with a full 32-bit word in which every bit is 1.
-pub fn input(um: &mut UniversalMachine, C: &u32) {
+pub fn input(um: &mut UniversalMachine, r_c: &u32) {
     match stdin().bytes().next() {
-        Some(value) => um.registers[*C as usize] = value.unwrap() as u32,
-        None => um.registers[*C as usize] = !0 as u32
+        Some(value) => um.registers[*r_c as usize] = value.unwrap() as u32,
+        None => um.registers[*r_c as usize] = 1 as u32
       }
 }
 
@@ -122,20 +113,20 @@ pub fn input(um: &mut UniversalMachine, C: &u32) {
 // $m[0][$r[C]]. If $r[B]=0, the load program
 // operation should be extremely quick, as this is
 // effectively a jump.
-pub fn load_prog(um: &mut UniversalMachine, B: &u32, C: &u32) {
-    if um.registers[*B as usize] == 0 {
-        um.program_counter = um.registers[*C as usize] as usize;
+pub fn load_prog(um: &mut UniversalMachine, r_b: &u32, r_c: &u32) {
+    if um.registers[*r_b as usize] == 0 {
+        um.program_counter = um.registers[*r_c as usize] as usize;
       }
       else{
-        *um.mem_segs.get_mut(0).unwrap() = um.mem_segs[um.registers[*B as usize] as usize].clone();
-        um.program_counter = um.registers[*C as usize] as usize;
+        *um.mem_segs.get_mut(0).unwrap() = um.mem_segs[um.registers[*r_b as usize] as usize].clone();
+        um.program_counter = um.registers[*r_c as usize] as usize;
       }
 }
 
 // $r[A] := value of least significant 25 bits of $r[B]
 pub fn load_val(um: &mut UniversalMachine, word: u32) {
-    let X = get(&RL, &word);
-    let Y = get(&VL, &word);
+    let index = get(&RL, &word);
+    let value = get(&VL, &word);
 
-    um.registers[X as usize] = Y;
+    um.registers[index as usize] = value;
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,311 @@
 mod tests {
     use crate::um::UniversalMachine;
     use crate::instructions;
+    
+    #[test]
+    fn load_val_test() {
+        let mut um = UniversalMachine::new();
+        let val1: u32 = 0b_0000_0000_0000_0000_0000_0000_0000_0001;
+        let val2: u32 = 0b_0000_0110_0000_0000_0000_0000_0000_0011;
+        let val3: u32 = 0b_0000_1100_1111_1111_1111_1111_1111_1111;
+        let val4: u32 = 0b_0000_1111_1111_1111_1111_1111_1111_1111;
+        instructions::load_val(&mut um, val1);
+        instructions::load_val(&mut um, val2);
+        instructions::load_val(&mut um, val3);
+        instructions::load_val(&mut um, val4);
+        assert_eq!(1, um.registers[0]);
+        assert_eq!(3, um.registers[3]);
+        assert_eq!(16777215, um.registers[6]);
+        assert_eq!(536870911, um.registers[7]); // not sure why this fails
+
+    }
+
+    #[test]
+    fn cmove_test() {
+        let mut um = UniversalMachine::new();
+        let r_1: u32 = 0b_0000_0010_0000_0000_0000_0000_0000_0000;
+        let r_2: u32 = 0b_0000_0100_0000_0000_0000_0000_0000_0011;
+        let r_3: u32 = 0b_0000_0110_0000_0000_0000_0000_0000_0001;
+        instructions::load_val(&mut um, r_1);
+        instructions::load_val(&mut um, r_2);
+        instructions::load_val(&mut um, r_3);
+        instructions::cmov(&mut um, &1, &2, &3);
+        assert_eq!(3, um.registers[1]);
+
+        let r_4: u32 = 0b_0000_1000_0000_0000_0000_0000_0000_0000;
+        let r_5: u32 = 0b_0000_1010_0000_0000_0000_0000_0000_0011;
+        let r_6: u32 = 0b_0000_1100_0000_0000_0000_0000_0000_0001;
+        instructions::load_val(&mut um, r_4);
+        instructions::load_val(&mut um, r_5);
+        instructions::load_val(&mut um, r_6);
+        instructions::cmov(&mut um, &6, &5, &4);
+        assert_eq!(1, um.registers[6]);
+    }
+
+    #[test]
+    fn seg_store_test() {
+        let mut um = UniversalMachine::new();
+        let r_1: u32 = 0b_0000_0010_0000_0000_0000_0000_0000_0000;
+        let r_2: u32 = 0b_0000_0100_0000_0000_0000_0000_0000_0011;
+        let r_3: u32 = 0b_0000_0110_0000_0000_0000_0000_0000_0001;
+        //temp substitute for map_seg
+        um.mem_segs.pop();
+        um.mem_segs.push(vec![0; 4]);
+        instructions::load_val(&mut um, r_1);
+        instructions::load_val(&mut um, r_2);
+        instructions::load_val(&mut um, r_3);
+        instructions::seg_store(&mut um, &1, &2, &3);
+        assert_eq!(1, um.mem_segs[0][3]);
+    }
+    #[test]
+    fn seg_load_test() {
+        let mut um = UniversalMachine::new();
+        let r_1: u32 = 0b_0000_0010_0000_0000_0000_0000_0000_0000;
+        let r_2: u32 = 0b_0000_0100_0000_0000_0000_0000_0000_0011;
+        let r_3: u32 = 0b_0000_0110_0000_0000_0000_0000_0000_0001;
+        //temp substitute for map_seg
+        um.mem_segs.pop();
+        um.mem_segs.push(vec![0; 4]);
+        instructions::load_val(&mut um, r_1);
+        instructions::load_val(&mut um, r_2);
+        instructions::load_val(&mut um, r_3);
+        instructions::seg_store(&mut um, &1, &2, &3);
+        assert_eq!(1, um.mem_segs[0][3]);
+
+        // todo: figure out how to test seg_load
+    }
+
+    #[test]
+    fn add_test() {
+        let mut um = UniversalMachine::new();
+        let r_1: u32 = 0b_0000_0010_0000_0000_0000_0000_0000_0001;
+        let r_2: u32 = 0b_0000_0100_0000_0000_0000_0000_0000_0010;
+        instructions::load_val(&mut um, r_1);
+        instructions::load_val(&mut um, r_2);
+        instructions::add(&mut um, &0, &1, &2);
+        assert_eq!(3, um.registers[0]);
+    }
+
+    #[test]
+    fn mult_test() {
+        let mut um = UniversalMachine::new();
+        let r_1: u32 = 0b_0000_0010_0000_0000_0000_0000_0000_1111;
+        let r_2: u32 = 0b_0000_0100_0000_0000_0000_0000_0000_1111;
+        instructions::load_val(&mut um, r_1);
+        instructions::load_val(&mut um, r_2);
+        instructions::mul(&mut um, &0, &1, &2);
+        assert_eq!(225, um.registers[0]);
+    }
+
+    #[test]
+    fn div_test() {
+        let mut um = UniversalMachine::new();
+        let r_1: u32 = 0b_0000_0010_0000_0000_0000_0000_0000_1111;
+        let r_2: u32 = 0b_0000_0100_0000_0000_0000_0000_0000_0101;
+        instructions::load_val(&mut um, r_1);
+        instructions::load_val(&mut um, r_2);
+        instructions::div(&mut um, &0, &1, &2);
+        assert_eq!(3, um.registers[0]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn div_0_test() {
+        let mut um = UniversalMachine::new();
+        let r_1: u32 = 0b_0000_0010_0000_0000_0000_0000_0000_1111;
+        let r_2: u32 = 0b_0000_0100_0000_0000_0000_0000_0000_0000;
+        instructions::load_val(&mut um, r_1);
+        instructions::load_val(&mut um, r_2);
+        instructions::div(&mut um, &0, &1, &2);
+    }
+
+    #[test]
+    fn output_test() {
+        let mut um = UniversalMachine::new();
+        let r_1: u32 = 0b_0000_0000_0000_0000_0000_0000_0100_0001;
+        instructions::load_val(&mut um, r_1);
+        instructions::output(&mut um, &0);
+        assert_eq!('A', (um.registers[0] as u8) as char);
+    }
+///
+
+    #[test]
+    fn conditional_move_test() {
+        let mut um = UniversalMachine::new();
+
+        let val_reg_1: u32 = 0b_0000_0010_0000_1000_0001_0000_1100_0001; 
+        let val_reg_2: u32 = 0b_0000_0100_0000_0000_0000_0000_0000_1111; // 15
+
+        instructions::load_val(&mut um, val_reg_1);
+        instructions::load_val(&mut um, val_reg_2);
+
+        instructions::cmov(&mut um, &5, &2, &1);
+        assert_eq!(15, um.registers[2]);
+    }
+
+    #[test]
+    fn invalid_conditional_move_test() {
+        let mut um = UniversalMachine::new();
+
+        let val_reg_6: u32 = 0b_0000_1100_0000_0000_0000_0000_0000_0001;
+        let val_reg_1: u32 = 0b_0000_0010_0001_0010_1000_1000_0100_0001; // 68323393
+        
+        instructions::load_val(&mut um, val_reg_6);
+        instructions::load_val(&mut um, val_reg_1);
+        instructions::cmov(&mut um, &6, &1, &2);
+        assert_eq!(1, um.registers[6]);
+    }
+
+    #[test]
+    fn add_test1() {
+        let mut um = UniversalMachine::new();
+
+        let val_reg_1: u32 = 0b_0000_0010_0000_0000_0001_0000_0000_0001;
+        let val_reg_2: u32 = 0b_0000_0100_0000_0000_0000_0000_0000_0011;
+
+        instructions::load_val(&mut um, val_reg_1);
+        instructions::load_val(&mut um, val_reg_2);
+        instructions::add(&mut um, &3, &1, &2);
+        assert_eq!(4100, um.registers[3]);
+    }
+
+    #[test]
+    fn add_overflow_test() {
+        let mut um = UniversalMachine::new();
+
+        um.registers[1] = u32::MAX;
+        um.registers[2] = 1;
+
+        instructions::add(&mut um, &3, &1, &2);
+        assert_eq!(0, um.registers[3]);
+    }
+
+    #[test]
+    fn load_value_test() {
+        let mut um = UniversalMachine::new();
+
+        let val_reg_1: u32 = 0b_0000_0010_0000_0000_0000_0000_0000_0001;
+
+        instructions::load_val(&mut um, val_reg_1);
+        assert_eq!(1, um.registers[1]);
+    }
+
+    #[test]
+    fn mult_test1() {
+        let mut um = UniversalMachine::new();
+
+        let val_reg_1: u32 = 0b_0000_0010_0000_0000_0000_0000_0000_0011; // 3
+        let val_reg_2: u32 = 0b_0000_0100_0000_0000_0000_0000_0000_0011; // 3
+
+        instructions::load_val(&mut um, val_reg_1);
+        instructions::load_val(&mut um, val_reg_2);
+        instructions::mul(&mut um, &3, &1, &2);
+        assert_eq!(9, um.registers[3]);
+    }
+
+    #[test]
+    fn mult_overflow_check_test() {
+        let mut um = UniversalMachine::new();
+
+        let val_reg_1: u32 = 0b_0000_0010_1111_1111_1111_1111_1111_1111;
+        let val_reg_2: u32 = 0b_0000_0100_1111_1111_1111_1111_1111_1111;
+
+        instructions::load_val(&mut um, val_reg_1);
+        instructions::load_val(&mut um, val_reg_2);
+        instructions::mul(&mut um, &3, &1, &2);
+        assert_eq!(4261412865, um.registers[3]);
+    }
+
+    #[test]
+    fn div_test1() {
+        let mut um = UniversalMachine::new();
+
+        let val_reg_1: u32 = 0b_0000_0010_1111_1111_1111_1111_1111_1111;
+        let val_reg_2: u32 = 0b_0000_0100_1111_1111_1111_1111_1111_1111;
+
+        instructions::load_val(&mut um, val_reg_1);
+        instructions::load_val(&mut um, val_reg_2);
+        instructions::div(&mut um, &3, &1, &2);
+        assert_eq!(1, um.registers[3]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn div_by_zero_test() {
+        let mut um = UniversalMachine::new();
+        let val_reg_1: u32 = 0b_0000_0010_1111_1111_1111_1111_1111_1111;
+
+        instructions::load_val(&mut um, val_reg_1);
+        instructions::div(&mut um, &6, &1, &2);
+    }
+
+    #[test]
+    fn map_seg_test() {
+        let mut um = UniversalMachine::new();
+
+        let three_reg_1: u32 = 0b_0000_0010_0000_0000_0000_0000_0000_0011;
+
+        // Load a value into a register      
+        // Load 3 into r[1]  
+        instructions::load_val(&mut um, three_reg_1);
+
+        // Map the segment we want to access
+        // B is the register index where we want map_seg to return the newly mapped segment index
+        instructions::map_seg(&mut um, &2, &1);
+
+        // So now, the index for our newly mapped segment should be in r[2]
+
+        // Now store the value in r[1] = 3, into the segment index in r[2]
+        instructions::seg_store(&mut um, &2, &0, &1);
+
+        // Load that segment's value into a register
+        // Load segments[r[2]][0] into r[5]
+        instructions::seg_load(&mut um, &3, &2, &0);
+
+        assert_eq!(um.registers[3], 3);
+    }
+
+    #[test]
+    fn map_seg_offset_test() {
+        let mut um = UniversalMachine::new();
+             
+        // Load r[1] with 3
+        let three_reg_1: u32 = 0b_0000_0010_0000_0000_0000_0000_0000_0011;
+        instructions::load_val(&mut um, three_reg_1);
+
+        // Load r[3] with 0
+        let zero_reg_3: u32 = 0b_0000_0110_0000_0000_0000_0000_0000_0000;
+        instructions::load_val(&mut um, zero_reg_3);
+
+        // Load r[4] with 1
+        let one_reg_4: u32 = 0b_0000_1000_0000_0000_0000_0000_0000_0001;
+        instructions::load_val(&mut um, one_reg_4);
+
+        // Load r[5] with 2
+        let two_reg_5: u32 = 0b_0000_1010_0000_0000_0000_0000_0000_0010;
+        instructions::load_val(&mut um, two_reg_5);
+
+        // Map the segment we want to access
+        // r[B] is the register where we want map_seg to return the newly mapped segment index
+        // r[C] is the capacity of the new segment
+        instructions::map_seg(&mut um, &2, &5);
+
+        // Value we want to check for:                r[1] = 3 
+        // Index of newly mapped segment:             r[2]
+        // Value of 0th segment offset:               r[3] = 0
+        // Value of 1st segment offset:               r[4] = 1
+        // Capacity to give map_seg for new segment : r[5] = 2
+
+        // Store the value in r[1] = 3, in segment[r[2]][r[3] = 0]
+        instructions::seg_store(&mut um, &2, &3, &1);
+
+        // Store the value in r[1] = 3, in segment[r[2]][r[4] = 1]
+        instructions::seg_store(&mut um, &2, &4, &1);
+
+        // Load seg[2][1] into r[6]
+        instructions::seg_load(&mut um, &6, &2, &4);
+
+        assert_eq!(um.registers[6], 3);
+    }
+
 }
