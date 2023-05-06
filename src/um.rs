e6a364bb54946a9185cd0982dbefd576a5983e9d
sathiarith <sathiarith_chau@uri.edu>
2023-05-06 01:58:43 -0400

    revised registers to array

--- src/um.rs
+++ src/um.rs
@@ -2,7 +2,7 @@ pub struct UniversalMachine {
 
     pub program_counter: usize,
     // The UM will only have 8 registers, each of which is a 32-bit word
-    pub registers: Vec<u32>,
+    pub registers: [u32; 8],
     pub mem_segs: Vec<Vec<u32>>,
     pub unmap_segs: Vec<u32>,
 
@@ -12,7 +12,7 @@ impl UniversalMachine {
     pub fn new() -> Self {
         Self {
             program_counter: 0,
-            registers: vec![0; 8],
+            registers: [0; 8],
             mem_segs: vec![vec![]],
             unmap_segs: vec![],
         }
